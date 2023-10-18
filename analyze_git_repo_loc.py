599da4eeca109cb7c72cbcbc048f7871e7a816b5
Nob Shinjo <90700567+nobShinjo@users.noreply.github.com>
2023-10-18 23:42:57 +0900

    fix: 月別、言語別に集約する

--- analyze_git_repo_loc.py
+++ analyze_git_repo_loc.py
@@ -7,6 +7,7 @@ import argparse
 import json
 import os
 import subprocess
+import sys
 from datetime import datetime, timedelta
 
 import pandas as pd
@@ -15,7 +16,7 @@ import plotly.express as px
 
 def run_cloc(
     repo_path: str, start_date: datetime, end_date: datetime, output_path: str
-):
+) -> str:
     """
     run_cloc 指定したgitリポジトリに対して、cloc解析する
 
@@ -27,34 +28,77 @@ def run_cloc(
 
     Returns:
         str: cloc解析結果(json形式)
+
+    Remarks:
+        git log コマンドで開始日付から終了日付までの変更を検索し、clocコマンドで詳細のLOCを解析する
+        cloc --by-file --json --git
     """
 
-    output_filename: str = os.path.join(
-        output_path, f"cloc_output_{end_date.strftime('%Y%m')}.json"
-    )
-    result = subprocess.run(
-        [
-            "cloc",
-            "--by-file",
-            "--json",
-            "--git",
-            f'--include-d={start_date.strftime("%Y-%m-%d")},{end_date.strftime("%Y-%m-%d")}',
-            f"--out={output_filename}",
-            repo_path,
-        ],
-        check=False,
-        capture_output=True,
-        text=True,
-    )
+    try:
+        git_log = subprocess.run(
+            [
+                "git",
+                "log",
+                f"--since={start_date.strftime('%Y-%m-%d')}",
+                f"--until={end_date.strftime('%Y-%m-%d')}",
+                "--name-only",
+                "--pretty=format:",
+            ],
+            check=True,
+            capture_output=True,
+            text=True,
+        )
+        if not git_log.stdout:
+            return ""
+
+        # git_log.stdoutから、空行は削除して、カンマ区切りのファイル名を連結いた文字列を作成する
+        files = ",".join(
+            [
+                file.replace(" ", "")
+                for file in git_log.stdout.split("\n")
+                if file.strip()
+            ]
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {str(e)}", file=sys.stderr)
+        sys.exit(-1)
+
+    if files is None:
+        return ""
+
+    try:
+        # output_filename: str = os.path.join(
+        #     output_path, f"cloc_output_{start_date.strftime('%Y%m')}.json"
+        # )
+        result = subprocess.run(
+            [
+                "./cloc.exe",
+                "--by-file",
+                "--json",
+                "--vcs=git",
+                f"--list-file={files}",
+                repo_path,
+            ],
+            check=True,
+            capture_output=True,
+            text=True,
+        )
+        if result.stdout:
+            print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {str(e)}", file=sys.stderr)
+        sys.exit(-1)
+
     return result.stdout
 
 
-def process_cloc_output(cloc_output: pd.DataFrame) -> pd.DataFrame:
+def process_cloc_output(cloc_output: pd.DataFrame, month: datetime) -> pd.DataFrame:
     """
     process_cloc_output clocコマンドのjson出力をpd.DataFrameに変換する
 
     Args:
         cloc_output (DataFrame): clocコマンドのjson形式の出力文字列
+        month (datetime): データ対称年月
 
     Returns:
         pd.DataFrame: (各言語名, LOCカウント数)のDataFrame
@@ -66,7 +110,9 @@ def process_cloc_output(cloc_output: pd.DataFrame) -> pd.DataFrame:
     data: pd.DataFrame = pd.DataFrame.from_dict(
         language_data, orient="index", columns=["code"]
     )
+    data = pd.DataFrame.from_dict(language_data, orient="index", columns=["code"])
     data["language"] = data.index
+    data["month"] = month
     return data
 
 
@@ -78,32 +124,38 @@ def plot_data(monthly_data: pd.DataFrame, output_path: str):
         monthly_data (DataFrame): 各月のLOC数データ
         output_path (str): ファイル出力先
     """
-    fig = px.bar(
-        monthly_data, x="language", y="code", title="LOC by Language per Month"
-    )
+
+    grouped_data = monthly_data.groupby(["month", "language"]).sum().reset_index()
+    pivot_data = grouped_data.pivot(index="month", columns="language", values="code")
+    fig = px.area(pivot_data, title="LOC by Language per Month")
     fig.show()
     fig.write_html(os.path.join(output_path, "report.html"))
 
 
 def analyze_git_repo_loc(
-    repo_path: str, start_date: datetime, end_date: datetime, output_path: str
+    repo_path: str, start_date_str: str, end_date_str: str, output_path: str
 ) -> pd.DataFrame:
     """
     analyze_git_repo_loc Gitリポジトリ内のLOC（Lines Of Code）を分析し、指定された日付範囲内の変更量を計算します。
 
     Parameters:
         repo_path (str): Gitリポジトリへのパス
-        start_date (datetime): 開始日付
-        end_date (datetime): 終了日付
+        start_date (str): 開始日付
+        end_date (str): 終了日付
         output_path (str): ファイル出力先
 
     Returns:
         loc_dict (DataFrame): 各月のLOC数データ
     """
-    start_date = datetime.strptime(start_date, "%Y-%m-%d")
-    end_date = datetime.strptime(end_date, "%Y-%m-%d")
+    try:
+        start_date: datetime = datetime.strptime(start_date_str, "%Y-%m-%d")
+        end_date: datetime = datetime.strptime(end_date_str, "%Y-%m-%d")
+    except ValueError as e:
+        print(f"Error: {str(e)}", file=sys.stderr)
+        sys.exit(-1)
+
     current_date: datetime = start_date
-    all_data: pd.DataFrame = None
+    all_data: pd.DataFrame = pd.DataFrame()
 
     while current_date <= end_date:
         next_date: datetime = (
@@ -115,12 +167,12 @@ def analyze_git_repo_loc(
             end_date=next_date,
             output_path=output_path,
         )
-        monthly_data: pd.DataFrame = process_cloc_output(cloc_output)
-        monthly_data["month"] = current_date.strftime("%Y-%m")
-        if all_data is None:
-            all_data = monthly_data
-        else:
-            all_data = pd.concat([all_data, monthly_data])
+        monthly_data: pd.DataFrame = pd.DataFrame()
+        if cloc_output:
+            monthly_data = process_cloc_output(
+                cloc_output=cloc_output, month=current_date.strftime("%Y-%m")
+            )
+        all_data = pd.concat([all_data, monthly_data])
         current_date = next_date
     return all_data
 
@@ -139,13 +191,13 @@ args = parser.parse_args()
 
 # 出力先ディレクトリ作成
 if not os.path.exists(args.output):
-    os.mkdir(args.output)
+    os.makedirs(args.output, exist_ok=True)
 
 # gitリポジトリ解析
 loc_data: pd.DataFrame = analyze_git_repo_loc(
     repo_path=args.repo_path,
-    start_date=args.start_date,
-    end_date=args.end_date,
+    start_date_str=args.start_date,
+    end_date_str=args.end_date,
     output_path=args.output,
 )
 # Chart出力
