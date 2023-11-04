f0280e01813ada00d901960051acd6f26ae93cb5
Nob Shinjo <90700567+nobShinjo@users.noreply.github.com>
2023-11-04 21:03:21 +0900

    fix: dataframeの加工

--- analyze_git_repo_loc.py
+++ analyze_git_repo_loc.py
@@ -52,13 +52,8 @@ def run_cloc(
             return ""
 
         # git_log.stdoutから、空行は削除して、カンマ区切りのファイル名を連結いた文字列を作成する
-        files = ",".join(
-            [
-                file.replace(" ", "")
-                for file in git_log.stdout.split("\n")
-                if file.strip()
-            ]
-        )
+        files = ",".join([file for file in git_log.stdout.split("\n") if file.strip()])
+
     except subprocess.CalledProcessError as e:
         print(f"Error: {str(e)}", file=sys.stderr)
         sys.exit(-1)
@@ -73,7 +68,7 @@ def run_cloc(
         result = subprocess.run(
             [
                 "./cloc.exe",
-                "--by-file",
+                "--by-file-by-lang",
                 "--json",
                 "--vcs=git",
                 f"--list-file={files}",
@@ -104,15 +99,12 @@ def process_cloc_output(cloc_output: pd.DataFrame, month: datetime) -> pd.DataFr
         pd.DataFrame: (各言語名, LOCカウント数)のDataFrame
     """
     data_dict = json.loads(cloc_output)
-    language_data = data_dict["SUM"]
+    language_data = data_dict["by_lang"]
     language_data.pop("header", None)
     language_data.pop("nFiles", None)
-    data: pd.DataFrame = pd.DataFrame.from_dict(
-        language_data, orient="index", columns=["code"]
-    )
     data = pd.DataFrame.from_dict(language_data, orient="index", columns=["code"])
-    data["language"] = data.index
     data["month"] = month
+    data["language"] = data.index
     return data
 
 
@@ -127,7 +119,13 @@ def plot_data(monthly_data: pd.DataFrame, output_path: str):
 
     grouped_data = monthly_data.groupby(["month", "language"]).sum().reset_index()
     pivot_data = grouped_data.pivot(index="month", columns="language", values="code")
-    fig = px.area(pivot_data, title="LOC by Language per Month")
+    fig = px.area(
+        data_frame=pivot_data,
+        x="month",
+        y="loc",
+        title="LOC by Language per Month",
+        color="language",
+    )
     fig.show()
     fig.write_html(os.path.join(output_path, "report.html"))
 
@@ -177,28 +175,32 @@ def analyze_git_repo_loc(
     return all_data
 
 
-# コマンドライン引数解析
-parser = argparse.ArgumentParser(
-    prog="Analyze Git Repository LOC",
-    description="Analyze Git repositories and visualize code LOC.",
-)
-# pylint: enable=line-too-long
-parser.add_argument("repo_path", type=str, help="Path of Git repository")
-parser.add_argument("-o", "--output", type=str, default="./out", help="Output path")
-parser.add_argument("-s", "--start_date", type=str, help="Start Date yyyy-mm-dd")
-parser.add_argument("-e", "--end_date", type=str, help="End Date yyyy-mm-dd")
-args = parser.parse_args()
+if __name__ == "__main__":
+    # コマンドライン引数解析
+    parser = argparse.ArgumentParser(
+        prog="Analyze Git Repository LOC",
+        description="Analyze Git repositories and visualize code LOC.",
+    )
+    # pylint: enable=line-too-long
+    parser.add_argument("repo_path", type=str, help="Path of Git repository")
+    parser.add_argument("-o", "--output", type=str, default="./out", help="Output path")
+    parser.add_argument("-s", "--start_date", type=str, help="Start Date yyyy-mm-dd")
+    parser.add_argument("-e", "--end_date", type=str, help="End Date yyyy-mm-dd")
+    args = parser.parse_args()
 
-# 出力先ディレクトリ作成
-if not os.path.exists(args.output):
-    os.makedirs(args.output, exist_ok=True)
+    # 出力先ディレクトリ作成
+    output_dir: str = os.path.join(
+        os.getcwd().replace(os.sep, "/"), args.output
+    ).replace(os.sep, "/")
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir, exist_ok=True)
 
-# gitリポジトリ解析
-loc_data: pd.DataFrame = analyze_git_repo_loc(
-    repo_path=args.repo_path,
-    start_date_str=args.start_date,
-    end_date_str=args.end_date,
-    output_path=args.output,
-)
-# Chart出力
-plot_data(monthly_data=loc_data, output_path=args.output)
+    # gitリポジトリ解析
+    loc_data: pd.DataFrame = analyze_git_repo_loc(
+        repo_path=args.repo_path,
+        start_date_str=args.start_date,
+        end_date_str=args.end_date,
+        output_path=output_dir,
+    )
+    # Chart出力
+    plot_data(monthly_data=loc_data, output_path=output_dir)
