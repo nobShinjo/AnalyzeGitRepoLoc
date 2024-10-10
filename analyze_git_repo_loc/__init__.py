56af75966d82c07967f508c143df5714e0abdc78
Nob Shinjo <90700567+nobShinjo@users.noreply.github.com>
2024-10-11 01:40:58 +0900

    Refactor __init__.py to update package metadata

---
+++ analyze_git_repo_loc/__init__.py
@@ -0,0 +1,16 @@
+"""
+analyze_git_repo_loc package.
+"""
+
+__version__ = "2.0.0"
+__author__ = "Nob Shinjo"
+__license__ = "MIT"
+__all__ = []
+
+from . import (
+    chart_builder,
+    colored_console_printer,
+    git_repo_loc_analyzer,
+    language_extensions,
+    utils,
+)
