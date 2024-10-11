4a578cf355fa1fa3e8cf0f8405e02b19e475c0ae
Nob Shinjo <90700567+nobShinjo@users.noreply.github.com>
2024-10-11 15:19:17 +0900

    Refactor LanguageComment class to include comment syntax for various programming languages

---
+++ analyze_git_repo_loc/language_comment.py
@@ -0,0 +1,82 @@
+class LanguageComment:
+    """
+    LanguageComment class is a class that contains the comment syntax for each language
+    """
+
+    language_comment_syntax = {
+        "C": [
+            "//",
+            "/*",
+        ],
+        "C++": [
+            "//",
+            "/*",
+        ],
+        "C#": [
+            "//",
+            "/*",
+        ],
+        "Java": [
+            "//",
+            "/*",
+        ],
+        "Python": [
+            "#",
+            "'''",
+        ],
+        "Ruby": [
+            "#",
+            "=begin",
+        ],
+        "Perl": [
+            "#",
+            "=pod",
+        ],
+        "PHP": [
+            "//",
+            "/*",
+        ],
+        "JavaScript": [
+            "//",
+            "/*",
+        ],
+        "TypeScript": [
+            "//",
+            "/*",
+        ],
+        "HTML": [
+            "<!--",
+        ],
+        "CSS": [
+            "/*",
+        ],
+        "SQL": [
+            "--",
+        ],
+        "Shell": [
+            "#",
+        ],
+        "Bash": [
+            "#",
+        ],
+        "PowerShell": [
+            "#",
+        ],
+        "R": [
+            "#",
+        ],
+    }
+    """ comment_dict is a dictionary that contains the comment syntax for each language """
+
+    @classmethod
+    def get_comment_syntax(cls, language: str) -> list[str]:
+        """
+        get_comment_syntax method returns the comment syntax for the given language
+
+        Args:
+            language (str): language name
+
+        Returns:
+            list[str]: comment syntax for the given language
+        """
+        return cls.language_comment_syntax.get(language, None)
